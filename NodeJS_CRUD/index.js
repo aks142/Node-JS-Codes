63d5d036e02188b4eff79a836896df0d71ca25c7
Ayush Singh <Ayush.Singh@IASYS.COM>
2024-02-28 15:13:26 +0530

    Node JS Code Folder commit

---
+++ NodeJS_CRUD/index.js
@@ -0,0 +1,16 @@
+
+const fs = require('fs') ;
+
+// fs.mkdirSync("Ayush");
+
+// fs.writeFileSync("Ayush/Bio.txt","Hello my name is Ayush Singh");
+
+// fs.appendFileSync("Ayush/Bio.txt",".\nI am a Node.js developer.");
+
+// const data = fs.readFileSync("Ayush/Bio.txt",{ encoding:"utf8", flag: "r" });
+// console.log(data);
+
+// fs.renameSync("Ayush/Bio.txt","Ayush/myBio.txt");
+
+// fs.rmSync("Ayush/myBio.txt",{ recursive:true,force:true });
+// fs.rmSync("Ayush",{ recursive:true,force:true });
